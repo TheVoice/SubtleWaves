560faa55e638591fb3b445225bd2698d1accc886
TheVoice <ras.p.voice@gmail.com>
2016-06-08 15:27:16 +0200

    Why are messages mandatory?!

--- src/AnswererWindow.java
+++ src/AnswererWindow.java
@@ -2,8 +2,14 @@ import javax.swing.JFrame;
 
 public class AnswererWindow extends JFrame{
 	
+	private AskerWindow myAsker = null;
+	
 	public AnswererWindow(){
 		initUI();
+	}
+	
+	public void setMyAskerWindow(AskerWindow aw){
+		myAsker = aw;
 	}
 
 	private void initUI(){
--- src/AskerWindow.java
+++ src/AskerWindow.java
@@ -1,12 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AskerWindow extends JFrame{
+	
+	private AnswererWindow myAnswerer = null;
 	
 	public AskerWindow(){
 		initUI();
 	}
+	
+	public void setMyAnswerer(AnswererWindow aw){
+		myAnswerer = aw;
+	}
 
 	private void initUI(){
+		Container pane = getContentPane();
+		
+		JPanel mainPanel = new JPanel();
+		//mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.PAGE_AXIS));
+		mainPanel.setLayout(new FlowLayout());
+		
+		JLabel label1 = new JLabel("Test");
+		mainPanel.add(label1);
+		
+		JTextField field1 = new JTextField();
+		mainPanel.add(field1);
+		
+		pane.add(mainPanel);
+		
 		setTitle("Subtle Waves - Questioning");
 		pack();
 		setLocationRelativeTo(null);
